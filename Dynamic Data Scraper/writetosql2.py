3f8ad24bbec9c93e7d6b4bb3bcc0d8070f5177c3
Conor Short <cshort@tcd.ie>
2020-02-12 15:36:17 +0000

    tidied writetosql2.py and added comments

--- Dynamic Data Scraper/writetosql2.py
+++ Dynamic Data Scraper/writetosql2.py
@@ -10,6 +10,8 @@ def unix_to_date(d):
     return datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
 
 
+
+
 api_key = "fc31aed31ee8e2ae5c2a3f75172b9167873f1bc9"
 URL = "https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=" + api_key
 
@@ -17,21 +19,13 @@ URL = "https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=" + api_k
 r = requests.get(url = URL)
 station_data = r.json()
 
-
-mydb = mysql.connector.connect(
-  host="dublin-bikes.cy2mnwcfkfbs.eu-west-1.rds.amazonaws.com",
-  user="admin",
-  passwd="fmRdzKkP6mTtwEEsCByh",
-  database="dublinbikes"
-)
-
 mycursor = mydb.cursor()
 
-sql_statement = ("INSERT IGNORE INTO `dublinbikes`.`dynamicinfo`"
-    " (`ID`, `availstands`, `availbikes`, `status`, `time`)"
-    " VALUES (%s, %s, %s, %s, %s)")
 
+# Create empty array to store values
 sql_values = []
+
+# Format the data for the sql statement and append to sql_values
 for station in station_data:
     sql_values.append((
         str(station["number"]),
@@ -40,10 +34,24 @@ for station in station_data:
         str(station["status"]),
         str(unix_to_date(station["last_update"])),))
 
+# Connect to the RDS database
+mydb = mysql.connector.connect(
+  host="dublin-bikes.cy2mnwcfkfbs.eu-west-1.rds.amazonaws.com",
+  user="admin",
+  passwd="fmRdzKkP6mTtwEEsCByh",
+  database="dublinbikes"
+)
+
+sql_statement = ("INSERT IGNORE INTO `dublinbikes`.`dynamicinfo`"
+    " (`ID`, `availstands`, `availbikes`, `status`, `time`)"
+    " VALUES (%s, %s, %s, %s, %s)")
+    
+# executemany iterates through the tuplse in sql_values array and applies the sql_statement to each tuple
 mycursor.executemany(sql_statement, sql_values)
 
+# Apply the changes to the server
 mydb.commit()
 
-print(mycursor.rowcount, "records inserted.")
+# Write a trace text file for when the file is scraping date. Will show time and number of records added
 f=open('C:/Users/Conor/Desktop/trace.txt', "a+")
 f.write(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "- " + str(mycursor.rowcount) + " records inserted.\n")
