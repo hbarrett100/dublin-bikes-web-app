363a8f9c85b1b9563f603bd6a4c2349f3a047edc
Evan McDonagh <evan.mc-donagh@ucdconnect.ie>
2020-02-13 10:36:20 +0000

    basic weather scraping

---
+++ Weather Scraper/get_weather.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+import requests 
+#import mysql.connector
+
+def unix_to_date(d):
+    """ Takes a unix timestamp as found in the dynamic data json and converts
+    to date and time for MySQL """
+
+    ts = int(d) / 1000
+    return datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+
+API_KEY = "16fb93e92d3bd8aefd9b647c1a8f6acf"
+URL = "http://api.openweathermap.org/data/2.5/weather?id=2172797&month=1&appid=" + API_KEY
+
+r = requests.get(url = URL)
+dublin_data = r.json()
+
+print(dublin_data)
