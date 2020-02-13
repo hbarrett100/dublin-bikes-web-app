bf74913767c717246f2e6def31c7aee45a312b5b
Evan McDonagh <evan.mc-donagh@ucdconnect.ie>
2020-02-13 10:52:21 +0000

    Fixed wrong city identification

--- Weather Scraper/get_weather.py
+++ Weather Scraper/get_weather.py
@@ -10,9 +10,10 @@ def unix_to_date(d):
     return datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
 
 API_KEY = "16fb93e92d3bd8aefd9b647c1a8f6acf"
-URL = "http://api.openweathermap.org/data/2.5/weather?id=2172797&month=1&appid=" + API_KEY
+URL = "http://api.openweathermap.org/data/2.5/weather?q=Dublin,ie&appid=" + API_KEY
 
 r = requests.get(url = URL)
 dublin_data = r.json()
 
-print(dublin_data)
+for attribute in dublin_data:
+    print(attribute + ": ", dublin_data[attribute])
