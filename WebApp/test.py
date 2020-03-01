96c954f5529e072d25fcf24af8500acc04779841
Conor Short <cshort@tcd.ie>
2020-03-01 19:12:26 +0000

    added simple map

--- WebApp/test.py
+++ WebApp/test.py
@@ -1,4 +1,35 @@
 from flask import Flask, render_template, url_for
+
+
+def get_locations():
+    import mysql.connector
+    sql_statement = ("select * FROM staticinfo")
+
+    try:
+        # Connect to the RDS database
+        mydb = mysql.connector.connect(
+            host="dublin-bikes.cy2mnwcfkfbs.eu-west-1.rds.amazonaws.com",
+            user="admin",
+            passwd="fmRdzKkP6mTtwEEsCByh",
+            database="dublinbikes"
+        )
+
+        mycursor = mydb.cursor()
+        mycursor.execute(sql_statement)
+
+        print(mycursor)
+    except mysql.connector.Error as err:
+
+        print("SOMETHING WENT WRONG:", err)
+
+    data = {}
+    for row in mycursor:
+        data[row[0]] = (row[3], row[4],)
+    mycursor.close()
+    mydb.close()
+
+    return data
+
 app = Flask(__name__)
 
 posts = [
@@ -16,6 +47,9 @@ posts = [
     }
 
 ]
+
+locationdata = [1,2,3]
+print(locationdata)
 
 @app.route('/')
 @app.route('/home')
